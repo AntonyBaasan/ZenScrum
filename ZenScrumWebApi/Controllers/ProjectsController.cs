9e55a08e6378c05ed3f5639eb8bdb200bce7635e
Antony Baasandorj <antony.baasan@gmail.com>
2017-11-10 15:09:43 -0500

    get single project

--- ZenScrumWebApi/Controllers/ProjectsController.cs
+++ ZenScrumWebApi/Controllers/ProjectsController.cs
@@ -5,12 +5,23 @@ namespace ZenScrumWebApi.Controllers
     [Route("api/[controller]")]
     public class ProjectsController : Controller
     {
-        // GET
+        [HttpGet("")]
         public IActionResult Index()
         {
-            return Ok(new {name = "Antony", age = "33"});
+            return Ok(new object[]
+            {
+                new {id = 1, name = "Antony", age = "33"},
+                new {id = 2, name = "Bold", age = "3"}
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Index(int id)
+        {
+            return Ok(new object[]
+            {
+                new {id = 1, name = "Antony", age = "33"}
+            });
         }
     }
-    
-    
 }
