3f19c184f5d0e52302775225fbd0a67539ff5a87
Antony Baasandorj <antony.baasan@gmail.com>
2017-11-10 14:46:47 -0500

    add project controller

---
+++ ZenScrumWebApi/Controllers/ProjectController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZenScrumWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok(new {name = "Antony", age = "33"});
+        }
+    }
+}
